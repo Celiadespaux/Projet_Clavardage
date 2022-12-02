81a969e745eef4091fc267592948428ac381f75c
Celiadespaux <celiadespaux@gmail.com>
2022-12-02 10:39:41 +0100

    tcp en cours

---
+++ src/main/java/transport/TCP.java
@@ -0,0 +1,56 @@
+package transport;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import model.*;
+
+
+
+
+public class TCP {
+	
+			//Pour plus tard
+			//String adresse_destinataire = Destinataire.getIp();
+			//int port = Expediteur.getPort();
+		
+
+	
+	
+	public void envoyer_msg_tcp (User Destinataire, User Expediteur, /*Message msg*/ String message1) throws UnknownHostException, IOException {	 
+		
+		String adresse_test = "127.0.0.1";
+		int port_test = 1024;
+		Socket link = new Socket(adresse_test,port_test);
+		
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(link.getOutputStream()));
+		
+		out.write(message1);
+		out.flush();
+		
+		link.close();
+	}
+	
+	
+	public void listen_msg_tcp () throws IOException {
+		
+		ServerSocket serveur = new ServerSocket(1024);
+		Socket link2 = serveur.accept();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(link2.getInputStream()));
+		
+		String recu = null;
+		while (recu == null) recu = in.readLine() ;
+		System.out.println(recu);
+		
+		link2.close();
+		serveur.close();
+	}
+	
+}
