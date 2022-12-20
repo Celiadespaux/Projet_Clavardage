7606c656a1d2928d0a67a2dfdd90360f23693971
echarpen <100856365+echarpen@users.noreply.github.com>
2022-12-20 14:37:51 +0100

    Affichage ConnexionWindow ok

--- src/main/java/view/ConnexionWindow.java
+++ src/main/java/view/ConnexionWindow.java
@@ -1,105 +1,83 @@
 package view;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class ConnexionWindow implements ActionListener {
-	
-	JFrame connexionFrame;
-	JPanel loginInfoPanel, connexionPanel;
-    JTextField idField, mdpField;
-	JLabel idLabel, mdpLabel, infoConnexion;
-	JButton connexionButton;
-	
-	public ConnexionWindow() {
-		
-		  
-        //Create and set up the window.
-		connexionFrame = new JFrame("Clavardage Connexion");
-		connexionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//connexionFrame.setMinimumSize(new Dimension(500, 200));
-		
-	    //Create and set up the panel.
-		loginInfoPanel = new JPanel(new GridLayout(2,2));
-		connexionPanel = new JPanel(new GridLayout(2,1));
-		
-		
-        //Add the widgets.
-        addWidgets();
+import java.io.IOException;
 
-        //Set the default button.
-		connexionFrame.getRootPane().setDefaultButton(connexionButton);
-        
-        //Add the panel to the window.
-		connexionFrame.getContentPane().add(loginInfoPanel, BorderLayout.CENTER);
-        connexionFrame.getContentPane().add(connexionPanel, BorderLayout.SOUTH);
-        
-        //Display the window.
-        connexionFrame.pack();
-        connexionFrame.setVisible(true);
+public class ConnexionWindow {
+
+    public void changeScene_ChoixPseudo(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(ConnexionWindow.class.getResource("/view/ChoixPseudoWindow.fxml"));
+        root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
-	
-	
-	/**
-     * Create and add the widgets.
-     */
-    private void addWidgets() {
-        //Create widgets.
-    	idField = new JTextField(2);
-    	mdpField =  new JTextField(2);
-    	idLabel = new JLabel("Id : ");
-    	mdpLabel = new JLabel("Mdp : ");
-    	infoConnexion = new JLabel("");
-    	connexionButton = new JButton("Connexion");
+    
+}
+/*
 
-        //Listen to events from the Convert button.
-    	connexionButton.addActionListener(this);
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-        //Add the widgets to the container.
-    	loginInfoPanel.add(idLabel);
-    	loginInfoPanel.add(idField);
-    	loginInfoPanel.add(mdpLabel);
-    	loginInfoPanel.add(mdpField);    	
-    	connexionPanel.add(connexionButton);
-    	connexionPanel.add(infoConnexion);
-        
-    	//idLabel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-    	//mdpLabel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConnexionWindow implements Initializable {
+
+    @FXML
+    private Button button_connexion;
+
+    @FXML
+    private TextField tf_id;
+
+    @FXML
+    private TextField tf_mdp;
+
+    public static void changeScene_ChoixPseudo(ActionEvent event) {
+        Parent root = null;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(ConnexionWindow.class.getResource("/view/ChoixPseudoWindow.fxml"));
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 1100, 700));
+        stage.show();
+
+
     }
-	
-		
-		
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		infoConnexion.setText("Connexion en cours");
-	}
-	
-	 /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-    	
-    	//Make sure we have nice window decorations.
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        
-        ConnexionWindow connexion = new ConnexionWindow();
-    }
-     
 
-	public static void main(String[] args) {
-		 //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_connexion.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                changeScene_ChoixPseudo(actionEvent);
             }
         });
 
-	}
+    }
+}*/
 
-
-}
