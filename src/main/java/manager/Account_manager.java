f36ff9d112233755a969fe2bf15ab38c3d92db31
echarpen <100856365+echarpen@users.noreply.github.com>
2023-01-23 16:57:45 +0100

    modifications notification 2

--- src/main/java/manager/Account_manager.java
+++ src/main/java/manager/Account_manager.java
@@ -62,7 +62,7 @@ public class Account_manager {
 		}
 	
 	public static void changer_pseudo(User sender, String newPseudo ) throws IOException {
-		String message = "L'utilisateur " + User.getMoi().getPseudo() + " a changé de pseudo en " + newPseudo;
+		String message = "Le nouveau pseudo de l'utilisateur " + User.getMoi().getPseudo() + " est " + newPseudo;
 		String msg = Message.construire_message(message,sender,Message.TypeMessage.CHANGE_PSEUDO);
 		Network_manager.Udp.broadcast(msg);
 		System.out.println("[MESSAGE] envoie du message -> je viens de changer mon pseudo");
